5518e13d3007dd1b634ada318e709109ce099eb4
Peter Grunde <peter.a.grunde@gmail.com>
2014-09-08 11:33:09 -0600

    all tasks completed but my tests are still borked

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,10 +3,18 @@ class Prescription < ActiveRecord::Base
 
   validates_presence_of :medication, :dosage, :schedule, :starts_on, :ends_on
   validate :ends_later_than_starts?
+  validate :dosage_must_start_with_numbers
 
   def ends_later_than_starts?
     if starts_on > ends_on
       errors.add(:starts_on, "must be before Ends on")
+    end
+  end
+
+  def dosage_must_start_with_numbers
+    first_text = dosage.split(" ").first
+    if first_text.to_i == 0
+      errors.add(:dosage, "must start with an integer")
     end
   end
 end
